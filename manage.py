a1c3807708f90110f323b17a813e5d1d276e1b9b
YoungBaeeee <jjolba@naver.com>
2017-06-12 21:12:24 +0000

    settings.py set by operating environment

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "h2me.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "h2me.settings.dev")
     try:
         from django.core.management import execute_from_command_line
     except ImportError:
