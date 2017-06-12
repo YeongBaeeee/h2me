fd04b7bc953fb349a4c22e2270aabfa19eb2cec9
YoungBaeeee <jjolba@naver.com>
2017-06-12 23:06:56 +0000

    Add user and status to diary.models

--- diary/models.py
+++ diary/models.py
@@ -1,12 +1,19 @@
 # diary/models.py
 
 from django.db import models
+from django.conf import settings
 from django.utils import timezone
 
 class Diary(models.Model):
+    STATUS_CHOICES = (
+        ('s', 'Secret'),
+        ('p', 'Public'),
+    )
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='diary_set')
     content = models.CharField(max_length=50, help_text='사진으로 대체')
     tags = models.CharField(max_length=50, blank=True)
     lnglat = models.CharField(max_length=50, blank=True, help_text='위젯으로 대체')
+    status = models.CharField(max_length=1, choices=STATUS_CHOICES)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
